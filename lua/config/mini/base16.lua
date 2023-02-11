189251154380474cc10d2fa7e11c6a810153de14
Dominik Lutley <domlutley@gmail.com>
2023-02-11 01:05:10 +0100

    Fixed colors

--- lua/config/mini/base16.lua
+++ lua/config/mini/base16.lua
@@ -1,21 +1,26 @@
 require("mini.base16").setup({
-  palette = {
-    base00 = "#2b3339",
-    base01 = "#323c41",
-    base02 = "#503946",
-    base03 = "#868d80",
-    base04 = "#d3c6aa",
-    base05 = "#d3c6aa",
-    base06 = "#e9e8d2",
-    base07 = "#fff9e8",
-    base08 = "#7fbbb3",
-    base09 = "#d699b6",
-    base0A = "#83c092",
-    base0B = "#dbbc7f",
-    base0C = "#e69875",
-    base0D = "#a7c080",
-    base0E = "#e67e80",
-    base0F = "#d699b6",
-  },
-  use_cterm = true,
+    palette = {
+        base00 = "#2b3339",
+        base01 = "#323c41",
+        base02 = "#503946",
+        base03 = "#868d80",
+        base04 = "#d3c6aa",
+        base05 = "#d3c6aa",
+        base06 = "#e9e8d2",
+        base07 = "#fff9e8",
+        -- base08 = "#7fbbb3", -- blue
+        base08 = "#e67e80", -- red
+        base09 = "#d699b6",
+        -- base0A = "#83c092", -- green
+        base0A = "#7fbbb3", -- blue
+        -- base0B = "#dbbc7f", -- yellow
+        base0B = "#a7c080", -- green
+        base0C = "#e69875", -- orange
+        -- base0D = "#a7c080", -- green
+        base0D = "#83c092", -- bluegreen
+        -- base0E = "#e67e80", -- red
+        base0E = "#dbbc7f", -- yellow
+        base0F = "#d699b6",
+    },
+    use_cterm = true,
 })
